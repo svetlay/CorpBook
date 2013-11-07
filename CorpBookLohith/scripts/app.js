c1ee14810304afea363f76bfb7929f43dc114fea
Lohith GNagaraj <kashyapa78@hotmail.com>
2013-11-07 12:28:05 -0600

    Got Logged In User Profile on Profile View Init Changes made by Lohith GNagaraj (kashyapa78@hotmail.com).

--- CorpBookLohith/scripts/app.js
+++ CorpBookLohith/scripts/app.js
@@ -7,9 +7,28 @@
     document.addEventListener('deviceready', function () {
         navigator.splashscreen.hide();
     }, false);
-
+    
     app.application = new kendo.mobile.Application(document.body, { layout: "tabstrip-layout", statusBarStyle: statusBarStyle, skin : "flat",initial:"Views/profile.html" });
 
+    var applicationSettings = {
+        emptyGuid: '00000000-0000-0000-0000-000000000000',
+        apiKey: 'VjJXja95aNJmqz0M' // set your API Key here
+    };
+    
+    localStorage["CorpBook.User"] = "Abhishek.Kant@telerik.com";
+    
+    // initialize Everlive SDK
+    app.el = new Everlive({
+        apiKey: applicationSettings.apiKey
+    });
+    
+    /*var filter = app.el.Query();
+    filter.where().eq('${Email}':'Abhishek.Kant@telerik.com');
+    */
+    
+    
+        
+    
     app.changeSkin = function (e) {
         if (e.sender.element.text() === "Flat") {
             e.sender.element.text("Native");
--- CorpBookLohith/scripts/profile.js
+++ CorpBookLohith/scripts/profile.js
@@ -1,4 +1,17 @@
 function profileInit(e)
 {
+    var loggedUser = localStorage["CorpBook.User"];
+
     
+    var filter = { 'Email': loggedUser};
+    
+    var data = app.el.data('Employee');
+    
+    data.get(filter)
+        .then(function(data){
+            console.log(JSON.stringify(data));
+        },
+        function(error){
+            console.log(JSON.stringify(error));
+        });
 }
