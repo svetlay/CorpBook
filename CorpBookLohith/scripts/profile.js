a15e7c2355b6e58d0a5e95dfcbfd07bc634e6e38
Abhishek Kant <abhishek@abhishekkant.net>
2013-11-07 13:47:09 -0600

    Profile show complete

--- CorpBookLohith/scripts/profile.js
+++ CorpBookLohith/scripts/profile.js
@@ -6,18 +6,23 @@ function profileInit(e)
     
     var data = app.el.data('Employee');
     var loggedUserPic,loggedUserView;
-        var loggedTemplate = new kendo.template($("#profileTemplate").html());
+   
+    var loggedTemplate = new kendo.template($("#profileTemplate").html());
     data.get(filter)
         .then(function(data){
             console.log(JSON.stringify(data));
-            loggedUserView = loggedTemplate(data.result[0]);
-            loggedUserPic = app.el.Files.getDownloadUrl(data.result[0].PictureFile);
-        },
+             loggedUserView = loggedTemplate(data.result[0]);
+           
+            console.log(loggedUserView);
+            $("#loggedUserDetails").html(loggedUserView);
+             loggedUserPic = app.el.Files.getDownloadUrl(data.result[0].PictureFile);
+            console.log(loggedUserPic);
+            $("#userPicture").attr("src",loggedUserPic);
+        },    
+  
         function(error){
             console.log(JSON.stringify(error));
         });
     
 
-
-    $("#loggedUserDetails").html(loggedUserView);
 }
