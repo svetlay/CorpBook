e9a4dc346ab26d27d0c545e139775cbca8ce6b71
Svetla Yankova <svetla.yankov@gmail.com>
2013-11-07 23:54:17 -0600

    Styling Updates to Profile View and Tabstrip Changes made by Svetla Yankova (svetla.yankov@gmail.com).

--- CorpBookLohith/scripts/profile.js
+++ CorpBookLohith/scripts/profile.js
@@ -1,25 +1,28 @@
-function profileInit(e) {
+function profileInit(e)
+{
     var loggedUser = localStorage["CorpBook.User"];
 
     var filter = { 'Email': loggedUser};
     
     var data = app.el.data('Employee');
-    var loggedUserPic,loggedUserView
+    var loggedUserPic,loggedUserView;
    
     var loggedTemplate = new kendo.template($("#profileTemplate").html());
     data.get(filter)
-    .then(function(data) {
-        console.log(JSON.stringify(data));
-        loggedUserView = loggedTemplate(data.result[0]);
+        .then(function(data){
+            console.log(JSON.stringify(data));
+             loggedUserView = loggedTemplate(data.result[0]);
            
-        $("#loggedUserDetails").html(loggedUserView);
-        loggedUserPic = app.el.Files.getDownloadUrl(data.result[0].PictureFile);
-        console.log(loggedUserPic);
-        $("#userPicture").attr("src", loggedUserPic);
-       
-    }, 
+            console.log(loggedUserView);
+            $("#loggedUserDetails").html(loggedUserView);
+             loggedUserPic = app.el.Files.getDownloadUrl(data.result[0].PictureFile);
+            console.log(loggedUserPic);
+            $("#userPicture").attr("src",loggedUserPic);
+        },    
   
-          function(error) {
-              console.log(JSON.stringify(error));
-          });
+        function(error){
+            console.log(JSON.stringify(error));
+        });
+    
+
 }
