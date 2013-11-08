4c8f96a5d690e9a49fcee222793b236a0733e064
Dhananjay Kumar <dhananjay.kumar@live.com>
2013-11-07 22:10:20 -0600

    Added LeaderBoard without Images working

--- CorpBookLohith/scripts/leaderboard.js
+++ CorpBookLohith/scripts/leaderboard.js
@@ -1,1 +1,25 @@
-﻿
+
+function leaderBoardInit()
+{
+    console.log("hello");
+  var leadersBoardDataSource = new kendo.data.DataSource({
+                                 type: 'everlive',
+                             transport: {
+                             typeName: 'Employee'
+                             },
+                             schema: {
+                             model: { id: Everlive.idField }
+                             },
+                              sort: { field: "GamerScore", dir: "desc" }
+                             
+                            });
+    
+  leadersBoardDataSource.fetch();
+  console.log(leadersBoardDataSource);
+  var leaderBoardTemplate =  new kendo.template($("#leaderboardtemplate").text());
+  $("#leaderboardlistview").kendoMobileListView({
+     dataSource: leadersBoardDataSource,
+     template: leaderBoardTemplate
+ });
+  
+};
