2bd4f03221474b3e3dc49dcdf69e3bf0ddcdced4
Svetla Yankova <svetla.yankov@gmail.com>
2013-11-07 23:02:49 -0600

    Changes made by Svetla Yankova (svetla.yankov@gmail.com).

--- CorpBookLohith/scripts/profile.js
+++ CorpBookLohith/scripts/profile.js
@@ -1,28 +1,25 @@
-function profileInit(e)
-{
+function profileInit(e) {
     var loggedUser = localStorage["CorpBook.User"];
 
     var filter = { 'Email': loggedUser};
     
     var data = app.el.data('Employee');
-    var loggedUserPic,loggedUserView;
+    var loggedUserPic,loggedUserView
    
     var loggedTemplate = new kendo.template($("#profileTemplate").html());
     data.get(filter)
-        .then(function(data){
-            console.log(JSON.stringify(data));
-             loggedUserView = loggedTemplate(data.result[0]);
+    .then(function(data) {
+        console.log(JSON.stringify(data));
+        loggedUserView = loggedTemplate(data.result[0]);
            
-            console.log(loggedUserView);
-            $("#loggedUserDetails").html(loggedUserView);
-             loggedUserPic = app.el.Files.getDownloadUrl(data.result[0].PictureFile);
-            console.log(loggedUserPic);
-            $("#userPicture").attr("src",loggedUserPic);
-        },    
+        $("#loggedUserDetails").html(loggedUserView);
+        loggedUserPic = app.el.Files.getDownloadUrl(data.result[0].PictureFile);
+        console.log(loggedUserPic);
+        $("#userPicture").attr("src", loggedUserPic);
+       
+    }, 
   
-        function(error){
-            console.log(JSON.stringify(error));
-        });
-    
-
+          function(error) {
+              console.log(JSON.stringify(error));
+          });
 }
