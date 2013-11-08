eca41f3bc2cf658e8270501a5e7de0d38ad37c9f
Abhishek Kant <abhishek@abhishekkant.net>
2013-11-08 05:07:17 -0600

    Added Camera Capabilities

--- CorpBookLohith/scripts/profile.js
+++ CorpBookLohith/scripts/profile.js
@@ -1,5 +1,4 @@
-function profileInit(e)
-{
+function profileInit(e) {
     var loggedUser = localStorage["CorpBook.User"];
 
     var filter = { 'Email': loggedUser};
@@ -9,20 +8,32 @@ function profileInit(e)
    
     var loggedTemplate = new kendo.template($("#profileTemplate").html());
     data.get(filter)
-        .then(function(data){
-            console.log(JSON.stringify(data));
-             loggedUserView = loggedTemplate(data.result[0]);
+    .then(function(data) {
+        console.log(JSON.stringify(data));
+        loggedUserView = loggedTemplate(data.result[0]);
            
-            console.log(loggedUserView);
-            $("#loggedUserDetails").html(loggedUserView);
-             loggedUserPic = app.el.Files.getDownloadUrl(data.result[0].PictureFile);
-            console.log(loggedUserPic);
-            $("#userPicture").attr("src",loggedUserPic);
-        },    
+        console.log(loggedUserView);
+        $("#loggedUserDetails").html(loggedUserView);
+        loggedUserPic = app.el.Files.getDownloadUrl(data.result[0].PictureFile);
+        console.log(loggedUserPic);
+        $("#userPicture").attr("src", loggedUserPic);
+    }, 
   
-        function(error){
-            console.log(JSON.stringify(error));
-        });
-    
+          function(error) {
+              console.log(JSON.stringify(error));
+          });
+}
 
+function capturePhoto() {
+    navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
+        quality: 50,
+        destinationType: destinationType.DATA_URL,
+        sourceType: 1
+    }
+    );
+}
+
+function onPhotoDataSuccess(imageData) {
+    var smallImage = document.getElementById('userPicture'); 
+    smallImage.src = "data:image/jpeg;base64," + imageData;
 }
